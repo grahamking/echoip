492c6b1701f06ae9b5d1b6245d421a7802df00d3
Graham King <graham@gkgk.org>
2014-05-17 16:55:38 -0400

    C client: Less includes, header comment.

--- client/ipi.c
+++ client/ipi.c
@@ -1,16 +1,25 @@
+/*
+ * IP Inquiry. Client for echoip. Displays your remote IP address and location.
+ *
+ * Usage: ipi [echoip-server]
+ * Default echoip-server is plebis.net
+ *
+ * Build it: make ipi
+ *
+ * https://github.com/grahamking/echoip
+*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/udp.h>
-#include <sys/types.h>
-#include <netdb.h>
-#include <string.h>
+#include <stdio.h>			// printf, fprintf, perror
+#include <netdb.h>			// getaddrinfo, gai_strerror
+#include <string.h>			// memset
 
 #define RECV_BUF_SIZE 80
 #define DEFAULT_SERVER "plebis.net"
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 
+// Send a single empty UDP packet to the server, and display contents of
+// first response packet.
 int main(int argc, char **argv)
 {
     int udp_socket, result;
@@ -30,26 +39,26 @@ int main(int argc, char **argv)
     result = getaddrinfo(server_ip, "7777", &addr_hints, &addr_result);
     if (result != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (udp_socket == -1) {
         perror("socket");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     result = sendto(
                  udp_socket, "", 0, 0, addr_result->ai_addr, addr_result->ai_addrlen);
     if (result == -1) {
         perror("sendto");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     received = recv(udp_socket, recv_buf, RECV_BUF_SIZE, 0);
     if (received == -1) {
-        perror("recvfrom");
-        exit(EXIT_FAILURE);
+        perror("recv");
+        return EXIT_FAILURE;
     }
 
     recv_buf[received] = '\0';
